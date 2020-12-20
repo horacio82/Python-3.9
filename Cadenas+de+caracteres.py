f04e623ada3d12a46983deadf55518201ec6da90
Horacio Molinari <molinarih2011@gmail.com>
2020-12-20 12:32:20 -0300

    Version_1

---
+++ Cadenas+de+caracteres.py
@@ -0,0 +1,35 @@
+
+cad1="hola"
+cad2="mundo"
+print(cad1 + " " +  cad2)
+
+cad3 = "hola "
+print(3 * cad3)
+
+long =len(cad2)
+print(long)
+
+long = len("")
+print(long)
+
+cadena= "Argentina"
+print("indexacion caracter 0: " , cadena[0])
+
+cadena= "Argentina"
+print("indexacion ultimo caracter: " , cadena[-1])
+
+cadena= "Argentina"
+print("Rango de la cadena : " , cadena[0:9])
+
+cadena= "Argentina"
+print("Rango de la cadena : " , cadena[:3])
+
+cadena= "Argentina"
+print("Rango de la cadena : " , cadena[4:])
+
+#cadena[4]="k"
+cadena="Argektina"
+print(cadena)
+
+subCad = cadena[2:5]
+print(subCad)
